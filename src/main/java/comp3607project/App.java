2e3a106f5ba2f86ca2272ba75c30f83cc5a702c6
Tyrell-Lewis <tyrell.lewis@my.uwi.edu>
2024-11-09 17:34:16 -0400

    gui, update

--- src/main/java/comp3607project/App.java
+++ src/main/java/comp3607project/App.java
@@ -4,8 +4,12 @@ public class App
 {
     public static void main(String[] args) 
     {
-        JudgePortal lennysPaymentPortal = new JudgePortal();
-        lennysPaymentPortal.setVisible(true);
+        //Judge system code goes here
+        //JudgeSystem judgeSystem = new JudgeSystem()
+        //in code line below, but the judgesystem in the brackets, new JudgePortal (judgeSystem)
+        JudgePortal autoJudgePortal = new JudgePortal();
+        
+        autoJudgePortal.setVisible(true);
     }
 }
 
