471208f400b7fb34c707c1efbb44b3403384338f
notjohnnym <jonathan.mohammed.15@gmail.com>
2024-11-10 04:39:26 -0400

    Added 2 Unit Tests

--- src/test/java/comp3607project/ChatBotPlatformTest.java
+++ src/test/java/comp3607project/ChatBotPlatformTest.java
@@ -2,12 +2,22 @@ package comp3607project;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.BeforeClass;
+import java.util.ArrayList;
+// import org.junit.BeforeClass;
 
 public class ChatBotPlatformTest extends TestCase {
-    
 
+    private final ChatBotPlatform testPlatform = new ChatBotPlatform();
+    
     public ChatBotPlatformTest() {}
+
+    @Test
+    public void testCreateChatBotPlatform() {
+        assertNotNull(new ChatBotPlatform());
+        assertTrue((new ChatBotPlatform()) instanceof ChatBotPlatform);
+        assertEquals(new ArrayList<ChatBot>(), testPlatform.getBots());
+    }
+
 
 
 }
--- src/test/java/comp3607project/ChatBotSimulationTest.java
+++ src/test/java/comp3607project/ChatBotSimulationTest.java
@@ -2,13 +2,17 @@ package comp3607project;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.BeforeClass;
+// import org.junit.BeforeClass;
 
 public class ChatBotSimulationTest extends TestCase {
     
     public ChatBotSimulationTest() {}
 
+    @Test
+    public void testCreateChatBotSimulation() {
+        assertNotNull(new ChatBotSimulation());
+        assertTrue((new ChatBotSimulation()) instanceof ChatBotSimulation);
+    }
 
-    
 
 }
