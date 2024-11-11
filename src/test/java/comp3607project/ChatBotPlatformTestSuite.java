39cd722724e7ba26b9ae9704d04d1d96dc27b793
v-runnn <varun.maharaj5@my.uwi.edu>
2024-11-11 17:35:10 -0400

    Finished up the test cases for all but the ChatBotSimulation class too. Testing a static main needs to be done so I have to figure that out.

--- src/test/java/comp3607project/ChatBotPlatformTestSuite.java
+++ src/test/java/comp3607project/ChatBotPlatformTestSuite.java
@@ -15,9 +15,22 @@ public class ChatBotPlatformTestSuite extends TestCase {
     public void testCreateChatBotPlatform() {
         assertNotNull(new ChatBotPlatform());
         assertTrue((new ChatBotPlatform()) instanceof ChatBotPlatform);
-        assertEquals(new ArrayList<ChatBot>(), testPlatform.getBots());
+        assertEquals(new ArrayList<ChatBot>(), testPlatform.getBots());//i dont know what this line is testing for.
     }
 
+    @Test
+    public void testAddChatBot() {
+        Boolean result = testPlatform.addChatBot(1);
+        assertNotNull(result,"Method must return a non null value");
+        assertTrue(result == true || result == false,"Method returned non boolean value");
+        assertTrue(testPlatform.getChatBotList().contains("ChatBot Name: ChatGPT-3.5"),"Incorrect Bot Created");
+    }
 
+    @Test
+    public void testInteractWithBot(){
+        String response = testPlatform.interactWithBot(0, "Hello World");
+        assertNotNull(response,"Method must return a non null value");
+        assertTrue(response.contains("Incorrect Bot Number")||response.contains("Daily Limit Reached. Wait 24 hours to resume chatbot usage")||(response.contains("(Message#") && response.contains(")") && response.contains("Response from") && response.contains(">>")), "Method must adhere to the output format provided");
+    }
 
 }
