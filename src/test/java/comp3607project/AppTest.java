9a94032534d19a3c137ee48b80bb35359a043581
amirpersad <126283878+amirpersad@users.noreply.github.com>
2024-11-11 07:37:47 +0000

    tests up to invoker completed - some tests require minor changes to test class (noted in comments)

--- src/test/java/comp3607project/AppTest.java
+++ src/test/java/comp3607project/AppTest.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipOutputStream;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Unit test for simple App.
@@ -41,7 +43,78 @@ public class AppTest {
     // Specialized Tests for Command Classes execute function -- To be Done tmr if not finished today
 
 
-    //
+    //Test for Files
+    @Test
+    public void testCreateFileParser(){
+        FileManager manager = new FileManager();
+        FileIterator fileIterator = manager.createFileParser();
+        assertNotNull(fileIterator,"Returned object should not be null.");
+        assertTrue(fileIterator instanceof FileParser);
+    }
+    @Test
+    public void testGetExtension(){
+        FileManager manager = new FileManager();
+        assertTrue(manager.getExtension("testFile.java"), "Method should return true");
+        assertFalse(manager.getExtension("testFile.txt"), "Method should return false");
+        assertFalse(manager.getExtension("testFile.jv"), "Method should return false");
+    }
+    @Test // Minor additions needed to FileParser(getFiles)
+    public void testFileParserConstructor(){
+        List<FileType> files = new ArrayList<>();
+        files.add(new FileType("test1.java"));
+        files.add(new FileType("test2.java"));
+
+        FileParser fileParser = new FileParser(files);
+
+        assertEquals(files, fileParser.getFiles(), "Files should match");
+    }
+    @Test
+    public void testFileParserNext(){
+        List<FileType> files = new ArrayList<>();
+        files.add(new FileType("test1.java"));
+        files.add(new FileType("test2.java"));
+
+        FileParser fileParser = new FileParser(files);
+
+        assertEquals(files.get(0), fileParser.next(), "Should return first file");
+        assertEquals(files.get(1), fileParser.next(), "Should return second file");
+        assertNull(fileParser.next(), "Should return Null as there are no more files");
+    }
+    @Test
+    public void testFileParserHasNext(){
+        List<FileType> files = new ArrayList<>();
+        files.add(new FileType("test1.java"));
+        files.add(new FileType("test2.java"));
+
+        FileParser fileParser = new FileParser(files);
+
+        assertTrue(fileParser.hasNext(), "Should return true before first file");
+        fileParser.next();
+        assertTrue(fileParser.hasNext(), "Should return true after first file");
+        fileParser.next();
+        assertFalse(fileParser.hasNext(), "Should return false after last file");
+    }
+    @Test // need to add getFile to FileType
+    public void testFileTypeConstructor(){
+        File file = new File("test.java");
+        FileType fileType = new FileType(file);
+
+        assertEquals(file, fileType.getFile());
+    }
+    @Test
+    public void testGetAbsolutePath(){
+        File file = new File("test.java");
+        FileType fileType = new FileType(file);
+
+        assertEquals(file.getAbsolutePath(), fileType.getAbsolutePath());
+    }
+    @Test
+    public void testGetName(){
+        File file = new File("test.java");
+        FileType fileType = new FileType(file);
+
+        assertEquals(file.getName(), fileType.getName());
+    }
 
     @Test
     public void shouldAnswerWithTrue() {        
