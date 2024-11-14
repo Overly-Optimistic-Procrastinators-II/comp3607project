fd54d9495b95dda195e239ac0b0643ff29723ecc
notjohnnym <jonathan.mohammed.15@gmail.com>
2024-11-13 20:20:24 -0400

    Changed Test Result file to capture test output.

--- src/main/java/comp3607project/TestResult.java
+++ src/main/java/comp3607project/TestResult.java
@@ -1,15 +1,70 @@
 package comp3607project;
 
 public class TestResult {
-    // private String result;
-    // private enum status {PASS, FAIL};
-    // private int marks;
-    // private String comment;
-    // private String errorMessage;
+    private String testName;
+    private String description;
+    private String status;
+    private String comment;
+    private int mark;
 
-    public TestResult() {
-
+    public TestResult(String testName, String description, String status, int mark) {
+        this.testName = testName;
+        this.description = description;
+        this.status = status;
+        this.mark = mark;
+        this.comment = "";
     }
 
+    public TestResult(String testName, String description, String status, String comment, int mark) {
+        this.testName = testName;
+        this.description = description;
+        this.status = status;
+        this.comment = comment;
+        this.mark = mark;
+    }
+
+    public String getTestName() {
+        return this.testName;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
     
+    public String getStatus() {
+        return this.status;
+    }
+    
+    public String getComment() {
+        return this.comment;
+    }
+    
+    public int getMark() {
+        return this.mark;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public String toString() {
+        return (this.getStatus() + " " + this.getDescription() + " " + this.getMark() + " " +
+                this.getComment() + "\n");
+    }
 }
