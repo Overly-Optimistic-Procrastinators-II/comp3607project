c2a3504eb22b0091dedf8d443b55300a9fab75d7
notjohnnym <jonathan.mohammed.15@gmail.com>
2024-11-15 18:32:22 -0400

    Added DynamicJavaCompiler to compile submissions

---
+++ src/test/java/comp3607project/DynamicJavaCompiler.java
@@ -0,0 +1,20 @@
+package comp3607project;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+public class DynamicJavaCompiler {
+    public static void compile(String directory) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+        if(compiler == null) {
+            System.out.println("No java compiler available.\n");
+            System.out.println("Application requires a Java Development Kit (JDK) to be installed.\n");
+            return;
+        }
+
+        if(compiler.run(null, null, null, directory) != 0) {
+            System.out.println("Compilation failed");
+        }
+    }
+}
