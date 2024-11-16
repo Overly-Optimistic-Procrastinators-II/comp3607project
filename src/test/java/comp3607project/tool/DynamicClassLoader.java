b96e0e54409592d005b511e2044ff6f5bb40c54b
notjohnnym <jonathan.mohammed.15@gmail.com>
2024-11-16 03:55:15 -0400

    Updated ClassLoader and JavaCompiler

--- src/test/java/comp3607project/tool/DynamicClassLoader.java
+++ src/test/java/comp3607project/tool/DynamicClassLoader.java
@@ -1,9 +1,33 @@
 package comp3607project.tool;
 
-public class DynamicClassLoader implements BaseClassLoader {
-    
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ClassLoader;
+
+public class DynamicClassLoader extends ClassLoader implements BaseClassLoader {
+    private String classPath;
+
+    public DynamicClassLoader(String classPath) {
+        this.classPath = classPath;
+    }
 
     public Class<?> getClass(String name) throws ClassNotFoundException {
-        return Class.forName(name);
+        String filePath = classPath + File.separator + name.replace('.', File.separatorChar) + ".class";
+        File classFile = new File(filePath);
+
+        if(!classFile.exists()) {
+            throw new ClassNotFoundException("Class not found at " + filePath);
+        }
+
+        byte[] data;
+        try (InputStream inputStream = new FileInputStream(classFile)) {
+            data = inputStream.readAllBytes();
+        } catch (IOException e) {
+            throw new ClassNotFoundException("Error reading class file at " + filePath);
+        }
+
+        return defineClass(name, data, 0, data.length);
     }
 }
