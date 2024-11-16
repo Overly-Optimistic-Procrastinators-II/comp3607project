b9f2068ed062e984c1fc3d4100196437e77a39f1
notjohnnym <jonathan.mohammed.15@gmail.com>
2024-11-16 01:46:30 -0400

    Removed infos from JudgePortal and updated AppTest

--- src/test/java/comp3607project/AppTest.java
+++ src/test/java/comp3607project/AppTest.java
@@ -13,28 +13,32 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
-    @Test 
-    public void testJudgeSystem(){
+    @Test
+    public void shouldAnswerWithTrue() {        
+        assertTrue(true);
+    }
+    
+    
+     @Test 
+    public void testJudgeSystem() {
         assertNotNull(new JudgeSystem());
         assertTrue((new JudgeSystem()) instanceof JudgeSystem);
     }
 
+
     @Test 
-    public void testJudgePortal(){
+    public void testJudgePortal() {
         assertNotNull(new JudgePortal());
         assertTrue((new JudgePortal()) instanceof JudgePortal);
-    } 
+    }
+
 
     //General Test for Command Classes -- May require minor additions to the classes (getJudgeSystem and getFilePath)
     @Test
-    public void testCommandProducePDF(){
+    public void testCommandProducePDF() {
         JudgeSystem judgeSystem = new JudgeSystem();
-        String filePath;
-
         CommandProducePDF command = new CommandProducePDF(judgeSystem);
-
-        //assertEquals(judgeSystem, command.getJudgeSystem());
-        //assertEquals(filePath, command.getFilePath());
+        assertTrue(command instanceof CommandProducePDF); 
     }
 
     //Specialized Tests for Command Classes execute function -- To be Done tmr if not finished today
@@ -42,22 +46,25 @@ public class AppTest {
 
     //Test for Files -- No idea what to add for param for FileManager all other tests are passing
     @Test
-    public void testCreateFileParser(){
-        FileManager manager = new FileManager(null);
+    public void testCreateFileParser() {
+        FileManager manager = new FileManager(new File(""));
         FileIterator fileIterator = manager.createFileParser();
         assertNotNull(fileIterator,"Returned object should not be null.");
         assertTrue(fileIterator instanceof FileParser);
     }
+
+
     @Test
-    public void testGetExtension(){
-        FileManager manager = new FileManager(null);
+    public void testGetExtension() {
+        FileManager manager = new FileManager(new File(""));
         assertTrue(manager.getExtension("testFile.java"), "Method should return true");
         assertFalse(manager.getExtension("testFile.txt"), "Method should return false");
         assertFalse(manager.getExtension("testFile.jv"), "Method should return false");
     }
 
+
     @Test // Minor additions needed to FileParser(getFiles)
-    public void testFileParserConstructor(){
+    public void testFileParserConstructor() {
         List<FileType> files = new ArrayList<>();
         File testFile1 = new File("test1.java");
         File testFile2 = new File("test2.java");
@@ -68,8 +75,10 @@ public class AppTest {
 
         //assertEquals(files, fileParser.getFiles(), "Files should match");
     }
+
+
     @Test
-    public void testFileParserNext(){
+    public void testFileParserNext() {
         List<FileType> files = new ArrayList<>();
         File testFile1 = new File("test1.java");
         File testFile2 = new File("test2.java");
@@ -82,8 +91,10 @@ public class AppTest {
         assertEquals(files.get(1), fileParser.next(), "Should return second file");
         assertNull(fileParser.next(), "Should return Null as there are no more files");
     }
+
+
     @Test
-    public void testFileParserHasNext(){
+    public void testFileParserHasNext() {
         List<FileType> files = new ArrayList<>();
         File testFile1 = new File("test1.java");
         File testFile2 = new File("test2.java");
@@ -98,30 +109,31 @@ public class AppTest {
         fileParser.next();
         assertFalse(fileParser.hasNext(), "Should return false after last file");
     }
+
+
     @Test // need to add getFile to FileType
-    public void testFileTypeConstructor(){
+    public void testFileTypeConstructor() {
         File file = new File("test.java");
         FileType fileType = new FileType(file);
 
         //assertEquals(file, fileType.getFile());
     }
+
+
     @Test
-    public void testGetAbsolutePath(){
+    public void testGetAbsolutePath() {
         File file = new File("test.java");
         FileType fileType = new FileType(file);
 
         assertEquals(file.getAbsolutePath(), fileType.getAbsolutePath());
     }
+
+
     @Test
-    public void testGetName(){
+    public void testGetName() {
         File file = new File("test.java");
         FileType fileType = new FileType(file);
 
         assertEquals(file.getName(), fileType.getName());
-    }
-
-    @Test
-    public void shouldAnswerWithTrue() {        
-        assertTrue(true);
     }
 }
--- src/test/java/comp3607project/FileManager.java
+++ src/test/java/comp3607project/FileManager.java
@@ -7,37 +7,24 @@ package comp3607project;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
-// import java.io.IOException;
-// import java.nio.file.Files;
-// import java.nio.file.Path;
-// import java.nio.file.StandardCopyOption;
-// import java.util.zip.ZipEntry;
-// import java.util.zip.ZipInputStream;
 
 public class FileManager implements FileContainer {
     private List<FileType> files;
 
-    public FileManager (File dir)
-    {
+    public FileManager (File dir) {
         files = new ArrayList<FileType>();
         addingJavaFilesFromDirectory(dir);
     }
 
-    private void addingJavaFilesFromDirectory(File dir)
-    {
-        if (dir.isDirectory())
-        {
+    private void addingJavaFilesFromDirectory(File dir) {
+        if (dir.isDirectory()) {
             File [] fileList = dir.listFiles();
-            if (fileList != null)
-            {
-                for (File file : fileList)
-                {
-                    if (file.isFile() && getExtension(file.getName()))
-                    {
+            if (fileList != null) {
+                for (File file : fileList) {
+                    if (file.isFile() && getExtension(file.getName())) {
                         files.add(new FileType(file));
                     }
-                    else if (file.isDirectory())
-                    {
+                    else if (file.isDirectory()) {
                         addingJavaFilesFromDirectory(file);
                     }
                 }
@@ -45,24 +32,11 @@ public class FileManager implements FileContainer {
         }
     }
 
-    public FileIterator createFileParser() 
-    {
+    public FileIterator createFileParser() {
         return new FileParser(files);
     }
 
-    public boolean getExtension(String filePath)
-    {
-        String filename = filePath;
-        if (filename.endsWith(".java")) 
-        {
-            //System.out.println(filename + " is a .java file");
-            return true;
-        } else 
-        {
-            //System.out.println(filename + " is not a .java file");
-            return false;
-        }
+    public boolean getExtension(String filePath) {
+        return (filePath.endsWith(".java") ? true : false);
     }
-
-    
 }
