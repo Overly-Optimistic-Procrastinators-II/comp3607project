e74f77e44a5e0ebae169acdeb07d1115d3e4f735
v-runnn <varun.maharaj5@my.uwi.edu>
2024-11-16 21:19:39 -0400

    Modified the ChatBotSimulationTestSuite.java

--- src/test/java/comp3607project/suite/ChatBotSimulationTestSuite.java
+++ src/test/java/comp3607project/suite/ChatBotSimulationTestSuite.java
@@ -1,97 +1,101 @@
-// // package comp3607project.suite;
+package comp3607project.suite;
 
-// import java.io.ByteArrayOutputStream;
-// import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-// import org.junit.jupiter.api.AfterEach;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// // import comp3607project.tool.ClassHolder;
+// import comp3607project.tool.ClassHolder;
 
-// class ChatBotSimulationTestSuite {
+class ChatBotSimulationTestSuite {
     
-//     public ChatBotSimulationTestSuite() {}
-//     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-//     private final PrintStream standardOut = System.out;
-//     protected String output;
+    public ChatBotSimulationTestSuite() {}
 
-//     @BeforeEach
-//     @SuppressWarnings("unused")
-//     void setUp(){
-//         new ChatBotSimulation();
-//         System.setOut(new PrintStream(outputStreamCaptor));
-//         ChatBotSimulation.main(new String[]{});
-//         output = outputStreamCaptor.toString();
-//     }
+    private Class<?> ChatBotSimulation;
+    
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream standardOut = System.out;
+    protected String output;
 
-//     @AfterEach
-//     @SuppressWarnings("unused")
-//     void tearDown(){
-//         System.setOut(standardOut);
-//         outputStreamCaptor.reset();
-//     }
+    @BeforeEach
+    @SuppressWarnings("unused")
+    void setUp() throws Exception{
+        // new ChatBotSimulation();
+        System.setOut(new PrintStream(outputStreamCaptor));
+        invokeMain();
+        output = outputStreamCaptor.toString();
+    }
+    
 
-//     @Test
-//     void testChatBotSimulation() {
-//         // 1 mark
-//         assertTrue(output.contains("Hello World"));
-//         assertEquals("Hello World!", output.lines().findFirst().get().trim());
-//     }
+    @AfterEach
+    @SuppressWarnings("unused")
+    void tearDown(){
+        System.setOut(standardOut);
+        outputStreamCaptor.reset();
+    }
 
 
-//     @Test
-//     void testCreateChatBotPlatform() {
-//         // 1 mark
-//         assertTrue(output.contains("Your ChatBots"));
-//         assertTrue(output.indexOf("Hello World!") < output.indexOf("Your ChatBots"));
-//     }
+    @Test
+    void testChatBotSimulation() {
+        // 1 mark
+        assertTrue(output.contains("Hello World"));
+        assertEquals("Hello World!", output.lines().findFirst().get().trim());
+    }
 
 
-//     @Test
-//     void testAddChatBotToPlatform() {
-//         // 2 marks
-//         // long errorCount = output.lines().filter(line -> line.contains("Error")).count();
-//         // assertEquals(0, errorCount, "There should be no errors when adding bots");
-
-// //         //This is supposed to return true since the program mark scheme did specify that you should add all the chatbots at least once
-// //         //TODO: ammend this test or the test subject
-// //         assertTrue(output.contains("ChatGPT-3.5") && output.contains("LLaMa") && output.contains("Mistral7B") && output.contains("Bard") && output.contains("Claude") && output.contains("Solar"));
-
-// //     }
+    @Test
+    void testCreateChatBotPlatform() {
+        // 1 mark
+        assertTrue(output.contains("Your ChatBots"));
+        assertTrue(output.indexOf("Hello World!") < output.indexOf("Your ChatBots"));
+    }
 
 
-//     @Test
-//     void testChatBotPlatformGetChatBotList() {
-//         // 2 marks
-//         assertTrue(output.contains("Bot Number"));
+    @Test
+    void testAddChatBotToPlatform() {
+        //2 marks
 
-// //         //TODO: cannot figure out how to get the other mark out of this one but you can tell what I was doing with the output cap and what not
-// //         //NewJeans Ditto might be the greatest song on this planet.
-// //     }
-
-
-//     @Test
-//     void testChatBotPlatfomInteractWithBot() {
-//         // 4 marks
-//         long firstInteractions = output.lines()
-//                             .filter(line->line.contains("Response from"))
-//                             .count();
-//         long secondInteractions = output.lines()
-//                                     .filter(line ->line.contains("Incorrect Bot Number") || line.contains("Daily Limit Reached. Wait 24 hours to resume chatbot usage"))
-//                                     .count();
-//         assertTrue(firstInteractions <=10, "There should be at most 10 interactions with a response message");
-//         assertTrue(secondInteractions >=5, "There should be at least 5 interactions with an Incorrect Bot message or a Daily limit message");
-//     }
+        //This is supposed to return true since the program mark scheme did specify that you should add all the chatbots at least once
+        //TODO: ammend this test or the test subject
+        assertTrue(output.contains("ChatGPT-3.5") && output.contains("LLaMa") && output.contains("Mistral7B") && output.contains("Bard") && output.contains("Claude") && output.contains("Solar"));
+    }
 
 
-//     @Test
-//     void testChatBotPlatformGetChatBotListUpdated() {
-//         // 2 marks
-//         assertTrue(output.contains("Your ChatBots"), "There should be a list of chatbots");
-//         assertTrue(output.lastIndexOf("Your ChatBots") != output.indexOf("Your ChatBots"), "There should be a second list of chatbots");
-//         assertTrue(output.indexOf("Your ChatBots")<output.lastIndexOf("Your ChatBots"));
-//     }
-// }
+    @Test
+    void testChatBotPlatformGetChatBotList() {
+        // 2 marks
+        //Cannot figure out how to get the other mark out of this one but you can tell what I was doing with the output cap and what not
+        assertTrue(output.contains("Bot Number"));
+    }
+
+
+    @Test
+    void testChatBotPlatfomInteractWithBot() {
+        // 4 marks
+        long firstInteractions = output.lines()
+                            .filter(line->line.contains("Response from"))
+                            .count();
+        long secondInteractions = output.lines()
+                                    .filter(line ->line.contains("Incorrect Bot Number") || line.contains("Daily Limit Reached. Wait 24 hours to resume chatbot usage"))
+                                    .count();
+        assertTrue(firstInteractions <=10, "There should be at most 10 interactions with a response message");
+        assertTrue(secondInteractions >=5, "There should be at least 5 interactions with an Incorrect Bot message or a Daily limit message");
+    }
+
+
+    @Test
+    void testChatBotPlatformGetChatBotListUpdated() {
+        // 2 marks
+        assertTrue(output.contains("Your ChatBots"), "There should be a list of chatbots");
+        assertTrue(output.lastIndexOf("Your ChatBots") != output.indexOf("Your ChatBots"), "There should be a second list of chatbots");
+        assertTrue(output.indexOf("Your ChatBots")<output.lastIndexOf("Your ChatBots"));
+    }
+
+    private void invokeMain() throws Exception {
+        ChatBotSimulation.getMethod("main", String[].class).invoke(null, (Object) new String[]{});
+    }
+}
